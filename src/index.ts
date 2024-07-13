180336d4a7e6e8b528decbd76174b0c278b41065
fwzyrln_ <165820519+fauziralpiandi@users.noreply.github.com>
2024-07-13 13:32:35 +0000

    push

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,31 @@
-import express from 'express';
-import path from 'path';
+import express from "express";
+import path from "path";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.post('/api/bmi', (req, res) => {
+app.post("/api/bmi", (req, res) => {
 	const { weight, height } = req.body;
 
 	if (!weight || !height) {
-		return res.status(400).json({ message: 'Berat dan Tinggi dibutuhkan' });
+		return res.status(400).json({ message: "Berat dan Tinggi dibutuhkan" });
 	}
 
 	const bmi = weight / (height * height);
-	let category = '';
+	let category = "";
 
 	if (bmi < 18.5) {
-		category = 'Kurus';
+		category = "Kurus";
 	} else if (bmi >= 18.5 && bmi < 24.9) {
-		category = 'Normal';
+		category = "Normal";
 	} else if (bmi >= 25 && bmi < 29.9) {
-		category = 'Berlebih';
+		category = "Berlebih";
 	} else {
-		category = 'Obesitas';
+		category = "Obesitas";
 	}
 
 	res.json({ bmi: bmi.toFixed(2), category });
